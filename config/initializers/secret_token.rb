6ea25ac178f355828d1a9ee9e695095bc89bb029
Mike Kreisher <mike.kreisher@gmail.com>
2012-03-23 23:49:00 -0400

    Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GumdoVocab::Application.config.secret_token = 'f03ddabffc7d42e14630665750d0126c842f27848f129f3bb058b3879a95ac3c212ebe6e81ca9ad4d8ad86df010558383ed8b9696b11321dedd844f68b28cb7f'
